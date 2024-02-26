f7a169689e2a1b3f039e3088ce972e7e2ed41a72
farmerrwl1 <161303378+farmerrwl1@users.noreply.github.com>
2024-02-27 00:03:37 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,26 +1,6 @@
 // db.js
 const Sequelize = require('sequelize');
 
-
-/*
-module.exports = new Sequelize(
-  "postgres://rivers_443g_user:YUqIjLJzwSB7XOlzYISR4DwIO4aBzYN9@dpg-cnef2ansc6pc73eg0scg-a.oregon-postgres.render.com/rivers_443g",
-  
-  {
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Только если ваш сервер не использует подписанный SSL-сертификат
-      },
-    },
-  }
-);
-
-*/
-
-
-
 const sequelize = new Sequelize('rivers_443g', 'rivers_443g_user', 'YUqIjLJzwSB7XOlzYISR4DwIO4aBzYN9', {
   host: 'dpg-cnef2ansc6pc73eg0scg-a',
   dialect: 'postgres',
