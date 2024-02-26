84597450a8685cc0d17f57cce1c35f924928d5b2
farmerrwl1 <161303378+farmerrwl1@users.noreply.github.com>
2024-02-26 23:08:56 +0300

    Update database.js

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('postgres', 'postgres', '123123', {
   host: 'localhost',
   dialect: 'postgres',
+  port: 5432,
 });
 
 module.exports = sequelize;
